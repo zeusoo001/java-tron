e2c858f58ccb19c2884d25432abd072b2170098b
zeusoo001 <zeus_oo@163.com>
2024-09-29 19:49:55 +0800

    a

--- framework/src/main/java/org/tron/program/DataProcess.java
+++ framework/src/main/java/org/tron/program/DataProcess.java
@@ -1,151 +1,168 @@
 package org.tron.program;
 
-import lombok.Data;
-import lombok.Setter;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import lombok.extern.slf4j.Slf4j;
-import org.tron.common.utils.JsonUtil;
-import org.tron.core.capsule.BytesCapsule;
-import org.tron.core.db.CommonStore;
+import org.bouncycastle.util.encoders.Hex;
+import org.tron.api.GrpcAPI;
+import org.tron.api.WalletGrpc;
+import org.tron.core.capsule.ContractCapsule;
+import org.tron.core.capsule.TransactionCapsule;
+import org.tron.program.datastat.Decode;
+import org.tron.program.datastat.Param;
+import org.tron.program.datastat.Result;
+import org.tron.protos.Protocol;
+import org.tron.protos.contract.SmartContractOuterClass;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Map;
+import java.util.List;
 import java.util.Set;
-
-
 
 @Slf4j(topic = "Data")
 public class DataProcess {
 
-    @Setter
-    private static CommonStore commonStore;
+    public static void init() throws Exception {
 
-    public static void init(CommonStore store){
-        commonStore = store;
-        load();
-    }
 
-    public static void load() {
 
-        try {
-            for(int kk = 1; kk <= 4; kk++) {
-                String path = "/data/trongrid-newlog/data/" + kk;
-//                String path = "/Users/adiswu/git/develop-1/java-tron/file";
+        Set<String> set = new HashSet<>();
+//    String path = "/data/test/java-tron/f";
+        String path = "/Users/adiswu/git/develop-1/java-tron/f";
 //
-                logger.info("path {}", path);
+        String ssss = "e3fb78d36509e9395704237bac7583cc880b6007f06a1421ebf754d97bab241d";
 
-                byte[] bytes = Files.readAllBytes(Paths.get(path));
+        logger.info("path {}", path);
 
-                logger.info("bytes {}", bytes.length);
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
 
-                String content = new String(bytes, StandardCharsets.UTF_8);
+        String content = new String(bytes, StandardCharsets.UTF_8);
 
-                String[] sz = content.split("\n");
+        String[] sz = content.split("\n");
 
-                logger.info("sz {}", sz.length);
+        for(int i = 0; i < sz.length; i++) {
+            if(ssss.equals(sz[i].trim())) {
+                logger.info("oooooooooooo");
+                break;
+            }
+            set.add(sz[i].trim());
+        }
 
-                int index = 0;
-                for(int i = 1; i < sz.length; i++) {
-                    try {
-                        if(sz[i].contains("api.fullnode") || sz[i].contains("api-limit.fullnode")) {
-                            String tmp = "";
-                            for (int n = index; n < i; n++) {
-                                tmp += sz[n];
-                            }
-                            process(tmp);
-                            index = i;
-                            if(sz[i].length() > 10) {
-                                logger.info("now process {}/{}", i, sz.length, sz[i].substring(0, 10));
-                            }else {
-                                logger.info("now process {}/{}", i, sz.length, sz[i]);
-                            }
+        logger.info("## set size {}", set.size());
 
-                        }
-                    }catch (Exception e){
-                        logger.info("process error 1 {}", e);
+        ManagedChannel channelFull2 = ManagedChannelBuilder.forTarget("127.0.0.1:50051")
+          .usePlaintext().build();
+//    ManagedChannel channelFull2 = ManagedChannelBuilder.forTarget("52.196.244.176:50051")
+//      .usePlaintext().build();
+
+        WalletGrpc.WalletBlockingStub blockingStub = WalletGrpc.newBlockingStub(channelFull2);
+
+        long startNum = 65626710;
+        long endNum = 65647996;
+
+        List<Result> results = new ArrayList<>();
+
+        Result tmp = new Result();
+        Param param = null;
+        for(long i = startNum; i < endNum; i++) {
+            try {
+                GrpcAPI.NumberMessage msg = GrpcAPI.NumberMessage.newBuilder().setNum(i).build();
+                Protocol.Block block = blockingStub.getBlockByNum(msg);
+
+                tmp.setBlockNum(block.getBlockHeader().getRawData().getNumber());
+                tmp.setBlockTime(block.getBlockHeader().getRawData().getTimestamp());
+                tmp.setWitness(Hex.toHexString(block.getBlockHeader().getRawData().getWitnessAddress().toByteArray()));
+
+                for(Protocol.Transaction transaction: block.getTransactionsList()) {
+                    TransactionCapsule capsule = new TransactionCapsule(transaction);
+                    int type = transaction.getRawData().getContract(0).getType().getNumber();
+                    if (type != Protocol.Transaction.Contract.ContractType.TriggerSmartContract_VALUE) {
+                        continue;
                     }
-                    logger.info("now process {}/{}", i, sz.length);
+
+                    SmartContractOuterClass.TriggerSmartContract triggerSmartContract = ContractCapsule.getTriggerContractFromTransaction(transaction);
+                    if (triggerSmartContract == null) {
+                        continue;
+                    }
+
+                    byte[] contractAddress = triggerSmartContract.getContractAddress().toByteArray();
+                    if (!Hex.toHexString(contractAddress).toUpperCase().equals("41FF7155B5DF8008FBF3834922B2D52430B27874F5")) {
+                        continue;
+                    }
+
+                    if (!transaction.getRet(0).toString().toLowerCase().contains("success")) {
+                        continue;
+                    }
+
+                    if(param != null) {
+                        Param p = Decode.decode(transaction);
+                        if(p.getPath().get(1).equals(param.getPath().get(1))){
+                            tmp.getTxs().add(transaction);
+                        }
+                    }
+
+                    if (!Hex.toHexString(capsule.getOwnerAddress()).toUpperCase().equals("41987C0191A1A098FFC9ADDC9C65D2C3D028B10CA3") ) {
+                        continue;
+                    }
+
+                    if(tmp.getLeftTx() == null) {
+                        tmp.setLeftTx(transaction);
+                        param = Decode.decode(transaction);
+                        continue;
+                    }
+
+                    if(tmp.getRightTx() == null) {
+                        tmp.setRightTx(transaction);
+                        break;
+                    }
                 }
 
-                logger.info("end process path {}", path);
+                int c1 = 0, c2 = 0, c3 = 0;
+                if (tmp.getRightTx() != null) {
+                    c1++;
+//          System.out.println((tmp.getRightTx() != null)
+//            + ", " + (tmp.getLeftTx() != null) + ", " + tmp.getTxs().size());
+//          System.out.println(tmp);
 
+                    if(tmp.getRightTx() != null && tmp.getLeftTx() != null && tmp.getTxs().size() > 0) {
+                        if(tmp.getTxs().size() == 1)c2++;
+                        results.add(tmp);
+                        String txid = new TransactionCapsule(tmp.getRightTx()).getTransactionId().toString();
+                        if(set.contains(txid)){
+                            logger.info("##### yes {}", txid);
+                        }else{
+                            logger.info("##### no {}，{}", txid, tmp.getTxs().size() == 1);
+                        }
+                        logger.info("{}", tmp);
+                    }
+                    tmp = new Result();
+                    param = null;
+
+//          String url = "http://44.230.214.237:8090/wallet/record?value=" + new TransactionCapsule(tmp.getTxs().get(0)).getTransactionId();
+//          System.out.println(url);
+//          URLConnection urlConnection = new URL(url).openConnection();
+//          BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+//          System.out.println(in.readLine());
+                }
+
+                logger.info("### c1 {}, c2 {}", c1, c2);
+
+            }catch (Exception e) {
+
+                System.out.println(e);
+                e.printStackTrace();
             }
 
-            logger.info("over process. ip-size {}", ips.size());
-
-            Map<String, Integer> map = new HashMap<>();
-            ips.forEach((k, v) -> {
-                commonStore.put(k.getBytes(), new BytesCapsule(JsonUtil.obj2Json(v).getBytes()));
-                map.put(k, v.size());
-            }) ;
-
-            commonStore.put("all-ip".getBytes(), new BytesCapsule(JsonUtil.obj2Json(map).getBytes()));
-        }catch (Exception e) {
-            logger.info("process error 2  {}", e);
         }
-    }
-
-    public static void main(String[] args) {
-        load();
-    }
-
-    @Data
-    public static class TxData {
-        private String txId;
-        private String apiKey;
-        private String ip;
-        private String time;
-        private String UA;
-    }
-
-    public static Map<String, Set<String>> ips = new HashMap<>();
-
-    public static void process(String s) {
-        TxData txData = get(s);
-        logger.info("{}", JsonUtil.obj2Json(txData));
-        if (txData == null || txData.getTxId().length() != 64) {
-            logger.info(s);
-            return;
-        }
-        commonStore.put(txData.getTxId().getBytes(), new BytesCapsule(JsonUtil.obj2Json(txData).getBytes()));
-
-        Set set = ips.get(txData.getIp());
-        if (set == null) {
-            set = new HashSet();
-            ips.put(txData.getIp(), set);
-        }
-        set.add(txData.getTxId());
-
-    }
 
 
 
-    public static TxData get(String s) {
-
-        try {
-            if (s.contains("api.fullnode")) {
-                TxData txData = new TxData();
-                txData.setIp(s.split(" - - ")[0].split("gz:")[1]);
-                txData.setTime(s.split("\\[")[1].split(" ")[0]);
-                txData.setTxId(s.split("tx_id: ")[1].split("\"")[0]);
-                txData.setApiKey(s.split("\"api.trongrid.io\" \"-\" \"")[1].split("\"")[0]);
-                txData.setUA(s.split("api.trongrid.io")[0].split("\"-\" \"")[1].split("\"")[0]);
-                return txData;
-            } else if (s.contains("api-limit.fullnode")) {
-                TxData txData = new TxData();
-                txData.setIp(s.split(" - - ")[0].split("gz:")[1]);
-                txData.setTime(s.split("\\[")[1].split(" ")[0]);
-                txData.setTxId(s.split("tx_id: ")[1].split("\"")[0]);
-                return txData;
-            }else {
-                logger.info(" onxxxx {} ", s.substring(0, 20));
-            }
-        } catch (Exception e) {
-            logger.info("process error 3 {}", e);
-        }
-        return null;
     }
 }
---
+++ framework/src/main/java/org/tron/program/datastat/TrxDetail.java
@@ -0,0 +1,27 @@
+package org.tron.program.datastat;
+
+import lombok.Data;
+
+import java.math.BigInteger;
+
+@Data
+public class TrxDetail {
+  private long blockNum;
+  private long blockTime;
+  private BigInteger amount;
+  private long callValue;
+  private String token;
+  private int index;
+  private boolean isBuy;
+  private String witness;
+  private String ret;
+
+  public String toString() {
+    StringBuilder b = new StringBuilder();
+    b.append(blockNum).append(",").append(blockTime).append(",")
+      .append(amount).append(",").append(callValue).append(",")
+      .append(token).append(",").append(index).append(",").append(isBuy).append(",").append(witness).append(",").append(ret);
+
+    return b.toString();
+  }
+}
