64b22adb72b667321643c1431310138954323787
zeusoo001 <zeus_oo@163.com>
2025-07-31 17:00:38 +0800

    A

--- framework/src/main/java/org/tron/core/config/args/Args.java
+++ framework/src/main/java/org/tron/core/config/args/Args.java
@@ -710,7 +710,7 @@ public class Args extends CommonParameter {
     PARAMETER.dnsPublishConfig = loadDnsPublishConfig(config);
 
     PARAMETER.syncFetchBatchNum = config.hasPath(Constant.NODE_SYNC_FETCH_BATCH_NUM) ? config
-        .getInt(Constant.NODE_SYNC_FETCH_BATCH_NUM) : 2000;
+        .getInt(Constant.NODE_SYNC_FETCH_BATCH_NUM) : 500;
     if (PARAMETER.syncFetchBatchNum > 2000) {
       PARAMETER.syncFetchBatchNum = 2000;
     }
